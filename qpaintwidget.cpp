dff77aaa9799c82a2cff4f9c0c7c55838fd8dfa5
aminbenarieb <amin.benarieb@gmail.com>
2016-03-24 15:02:49 +0300

    Added points to graphics.

--- qpaintwidget.cpp
+++ qpaintwidget.cpp
@@ -13,13 +13,18 @@ QPaintWidget::QPaintWidget(QWidget * parent) : QWidget(parent)
 void QPaintWidget::paintEvent(QPaintEvent *) {
 
     QPainter ppainter(this);
-    ppainter.setBrush(QBrush(Qt::transparent));
 
-    ppainter.setPen(QPen(Qt::red, kDrawPenWidth));
     ppainter.drawPoints(funcArray);
+    ppainter.setRenderHint(QPainter::Antialiasing, true);
 
     for (int i=0; i < funcArray.count()-1; i++)
+    {
+       ppainter.setPen(QPen(Qt::darkRed, kDrawPenWidth));
        ppainter.drawLine( QPointF(funcArray[i].x(), funcArray[i].y() + this->width()/2 ), QPointF(funcArray[i+1].x(), funcArray[i+1].y() + this->width()/2 ) );
+       ppainter.setPen(QPen(Qt::red, kDrawPenWidth));
+       ppainter.setBrush(QBrush(Qt::red));
+       ppainter.drawEllipse( QPointF(funcArray[i].x(), funcArray[i].y() + this->width()/2 ), kDrawPointRadius, kDrawPointRadius);
+    }
 
     ppainter.setPen(QPen(Qt::blue, kDrawPenWidth));
     ppainter.drawPoints(curveArray);
